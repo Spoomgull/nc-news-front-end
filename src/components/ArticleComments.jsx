23cc4162afe7c0b3a06bdfa92b62fe87ac666a39
Luke Berry <lukeberry12345@gmail.com>
2024-05-08 15:18:08 +0100

    completed task-7

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -8,6 +8,7 @@ function ArticleComments (){
     const articleChoice = article.article_id
     const [articleComments, setArticleComments] = useState([])
     const [loading, setLoading] = useState("loading")
+    
 
     useEffect(()=>{
         axios.get(`https://northcoders-news-api-bjpy.onrender.com/api/articles/${articleChoice}/comments`)
@@ -16,17 +17,17 @@ function ArticleComments (){
             setLoading("loaded")
     }
     ,[articleChoice])
-    console.log(articleComments)
     if(loading === "loaded"){
         return articleComments.map((comment)=>{
+
+
             return (
-                <button className="article-comment">
+                <button key={comment.comment_id}className="article-comment">
                     <li>Comment: {comment.body}</li>
                     <li>Posted by: {comment.author}</li>
                     <li>Likes: {comment.votes}</li>
                     <li>Posted at: {comment.created_at}</li>
                 </button>
-
             )
         })
 
--- src/components/ArticleContainer.jsx
+++ src/components/ArticleContainer.jsx
@@ -1,12 +1,14 @@
 import Article from "./Article"
+import UpdateArticleVotes from "./UpdateArticleVotes"
 import ArticleComments from "./ArticleComments"
 
 
 function ArticleContainer(){
     return (
         <>
-        <Article></Article>
-        <ArticleComments></ArticleComments>
+        <Article/>
+        <UpdateArticleVotes/>
+        <ArticleComments/>
         </>
     )
 }
